5ed82e1bd4a457e7c51f0d3c6a86a349c5cbd17e
Гриценко Богдан <0rbitrf0rmat@gmail.com>
2015-02-09 12:39:11 +0200

    BurningEffect

---
+++ PlaceBombFaster/PlaceBombFaster/BurningEffect.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace PlaceBombFaster
+{
+    class BurningEffect:GameEffect,IUpdatable,IDrawable
+    {
+        const int BurningDemage = 1;
+        public int DrawIndex { get; set; }
+
+
+        public BurningEffect(GamePhysicalObject target):base(target)
+        {
+            target.Effects.Add(this);
+            target.SetDamage(BurningDemage);
+        }
+
+        public void Update(TimeSpan timeSpan)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw(Canvas canvas)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
